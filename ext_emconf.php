c833532169adb721109aee41d3c2ac352b6e4261
Mathias Brodala <mbrodala@pagemachine.de>
2023-05-15 10:56:37 +0200

    [TASK] Allow installation with TYPO3v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
+            'typo3' => '10.4.0-12.4.99',
         ],
     ],
 ];
